6ca3c8fe6a5bd819aaa511260aa9020564e2030c
qichunlin <737795279@qq.com>
2019-04-04 17:58:26 +0800

    SQL DB

---
+++ tbl_dept.sql
@@ -0,0 +1,6 @@
+INSERT INTO `tbl_dept` VALUES (1, '开发部');
+INSERT INTO `tbl_dept` VALUES (2, '测试部');
+INSERT INTO `tbl_dept` VALUES (3, '财务部');
+INSERT INTO `tbl_dept` VALUES (4, '人事部');
+INSERT INTO `tbl_dept` VALUES (5, 'AMS');
+INSERT INTO `tbl_dept` VALUES (6, '国际事业部Global');
